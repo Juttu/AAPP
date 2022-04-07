888a42d844c35c6f30d0a9bd57fae4fb962f458c
Juttu <69361530+Juttu@users.noreply.github.com>
2022-04-07 14:20:36 +0530

    A7

--- server/common/config.js
+++ server/common/config.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto');
 
 exports.PORT = process.env.PORT || 8080;
-exports.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/app_db';
+exports.MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://Anurag:Anurag@cluster0.toske.mongodb.net/Waitlist?retryWrites=true&w=majority';
 exports.NODE_ENV = process.env.NODE_ENV || 'development';
-exports.OTP_SECRET = process.env.OTP_SECRET || crypto.randomBytes(64).toString('hex');
-exports.JWT_SECRET = process.env.JWT_SECRET || crypto.randomBytes(64).toString('hex');
+exports.OTP_SECRET = "1wES1i1gcaFhA65jk0Ea0f8XbnuEiiUo" || crypto.randomBytes(64).toString('hex');
+exports.JWT_SECRET = "Z3cwqPiWW21owQvOMxFYb9iMhXXJPnUA" || crypto.randomBytes(64).toString('hex');
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
-exports.F2S_API_KEY = process.env.F2S_API_KEY;
+exports.F2S_API_KEY = "Hu7vyw2WULaKPMNfrVFgdqETJoBcQnGeSjp6iXl891ZCstR0IhFBdib10hJItaRq43CK2W7uenSXMAD5";
